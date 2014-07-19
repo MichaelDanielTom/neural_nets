6eea9fa0e2df776ef9de8d6294a3f32fd4ce3605
PLDB Admin <pldbadmin@pldbcent.(none)>
2014-07-19 10:56:51 -0400

    T1: first commit, layed out planning groundwork

---
+++ neural_net.py
@@ -0,0 +1,19 @@
+"""
+	Utility functions for neural networks
+"""
+import numpy as np
+import json
+
+class NeuralNetwork(object):
+	"""
+		collection of nodes
+	"""
+	def __init__(self):
+		self.root = NeuralNetworkNode()
+
+class NeuralNetworkNode(object):
+	def __init__(self):
+
+
+if __name__ == '__main__':
+	NeuralNetwork()
